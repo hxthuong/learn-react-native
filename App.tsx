47c8225a7df990fea1f73590e8c23ddbc33da6b0
hxthuong <hothixuanthuong1408@gmail.com>
2025-07-25 16:44:11 +0700

    test TextInput

--- App.tsx
+++ App.tsx
@@ -1,12 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+    const [count, setCount] = useState<number>(0);
+    const [name, setName] = useState('');
+    const [age, setAge] = useState(0);
+
     return (
         <View style={styles.container}>
-            <Text style={styles.hello1}>Hello world 2!</Text>
-            <Text>Hello world!</Text>
-            <StatusBar style="auto" />
+            <View>
+                <Text style={styles.hello1}>Name: {name}</Text>
+                <TextInput
+                    style={styles.input}
+                    value={name}
+                    multiline
+                    autoCapitalize={'words'}
+                    onChangeText={setName}
+                />
+            </View>
+            <View>
+                <Text style={styles.hello1}>Age: {age}</Text>
+                <TextInput style={styles.input} value={age} keyboardType={'number-pad'} onChangeText={setAge} />
+            </View>
+            <Text style={styles.hello1}>{count}</Text>
+            <View>
+                <Button title="Increase" onPress={() => setCount(count + 1)} />
+            </View>
         </View>
     );
 }
@@ -18,11 +38,18 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
+    input: {
+        width: 320,
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+    },
     hello1: {
         color: 'red',
         fontSize: 40,
-        borderWidth: 1,
-        borderColor: 'red',
+        // borderWidth: 1,
+        // borderColor: 'red',
         padding: 10,
         fontWeight: '800',
     },
