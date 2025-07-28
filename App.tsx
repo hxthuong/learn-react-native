d1f17988d6ee0a7e8bf0caa65bf1bec99615dc4b
hxthuong <hothixuanthuong1408@gmail.com>
2025-07-28 08:37:22 +0700

    test FlatList

--- App.tsx
+++ App.tsx
@@ -1,33 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, FlatList, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
-    const [count, setCount] = useState<number>(0);
-    const [name, setName] = useState('');
-    const [age, setAge] = useState(0);
+    const [students, setStudents] = useState([
+        { id: 1, name: 'A', age: 10 },
+        { id: 2, name: 'B', age: 11 },
+        { id: 3, name: 'C', age: 12 },
+        { id: 4, name: 'D', age: 13 },
+        { id: 5, name: 'E', age: 14 },
+        { id: 6, name: 'F', age: 15 },
+        { id: 7, name: 'G', age: 10 },
+        { id: 8, name: 'H', age: 11 },
+        { id: 9, name: 'I', age: 12 },
+        { id: 10, name: 'K', age: 13 },
+        { id: 11, name: 'L', age: 14 },
+        { id: 12, name: 'M', age: 15 },
+    ]);
 
     return (
+        // <View style={styles.container}>
+        //     <Text style={styles.header}>Students</Text>
+        // </View>
         <View style={styles.container}>
-            <View>
-                <Text style={styles.hello1}>Name: {name}</Text>
-                <TextInput
-                    style={styles.input}
-                    value={name}
-                    multiline
-                    autoCapitalize={'words'}
-                    onChangeText={setName}
-                />
-            </View>
-            <View>
-                <Text style={styles.hello1}>Age: {age}</Text>
-                <TextInput style={styles.input} value={age} keyboardType={'number-pad'} onChangeText={setAge} />
-            </View>
-            <Text style={styles.hello1}>{count}</Text>
-            <View>
-                <Button title="Increase" onPress={() => setCount(count + 1)} />
-            </View>
+            <Text style={styles.header}>Students</Text>
+            <FlatList
+                data={students}
+                keyExtractor={(item) => `${item.id}`}
+                // numColumns={2}
+                renderItem={({ item }) => {
+                    return (
+                        <View>
+                            <Text style={styles.item}>
+                                STT: {item.id} - Tên: {item.name}
+                            </Text>
+                        </View>
+                    );
+                }}
+            />
         </View>
+        // <ScrollView style={styles.container}>
+        //     <Text style={styles.header}>Students</Text>
+        //     {students.map((item) => (
+        //         <Text style={styles.item} key={item.id}>
+        //             STT: {item.id} - Tên: {item.name}
+        //         </Text>
+        //     ))}
+        // </ScrollView>
     );
 }
 
@@ -35,8 +54,14 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+        // alignItems: 'center',
+        // justifyContent: 'center',
+    },
+    header: {
+        fontSize: 30,
+        fontWeight: '800',
+        marginVertical: 30,
+        textAlign: 'center',
     },
     input: {
         width: 320,
@@ -45,12 +70,12 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         padding: 10,
     },
-    hello1: {
-        color: 'red',
-        fontSize: 40,
-        // borderWidth: 1,
-        // borderColor: 'red',
-        padding: 10,
+    item: {
+        // width: '100%',
+        backgroundColor: 'lightblue',
+        padding: 30,
+        marginBottom: 30,
+        marginHorizontal: 20,
         fontWeight: '800',
     },
 });
